d7d9956537ebb165427b0d5b3f12f4dfafd67b4c
porto <pedrolportow@gmail.com>
2024-11-21 20:46:26 -0300

    added src folder and changed folder structure

---
+++ src/app/(unauthorized)/login/page.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import { z } from "zod";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Input } from "@ui/input";
+import { Button } from "@ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@ui/form";
+
+const FormSchema = z.object({
+  email: z.string().email({ message: "Por favor, insira um email válido." }),
+  password: z
+    .string()
+    .min(3, { message: "A senha deve ter pelo menos 3 caracteres." }),
+});
+
+export default function Login() {
+  const router = useRouter();
+
+  const form = useForm({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(data: z.infer<typeof FormSchema>) {
+
+  }
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-6 bg-white rounded shadow">
+        <h2 className="text-2xl font-bold text-center">Login</h2>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4"
+            noValidate
+          >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="usuario@gmail.com"
+                      type="email"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Senha</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Sua senha"
+                      type="password"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {form.formState.errors.root && (
+              <p className="text-red-500 text-sm">
+                {form.formState.errors.root.message}
+              </p>
+            )}
+
+            <Button type="submit" className="w-full">
+              Entrar
+            </Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+}
