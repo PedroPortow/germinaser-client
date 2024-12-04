428537d9804ff4a6262597294fdf0dcddd1618e6
porto <pedrolportow@gmail.com>
2024-12-04 20:24:08 -0300

    deps(Sentry): removed replay integration for sentry

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -9,9 +9,9 @@ Sentry.init({
   enabled: process.env.NODE_ENV === 'production', 
 
   // Add optional integrations for additional features
-  integrations: [
-    Sentry.replayIntegration(),
-  ],
+  // integrations: [
+  //   Sentry.replayIntegration(),
+  // ],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
@@ -19,10 +19,10 @@ Sentry.init({
   // Define how likely Replay events are sampled.
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+  // replaysSessionSampleRate: 0.1,
 
   // Define how likely Replay events are sampled when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
+  // replaysOnErrorSampleRate: 1.0,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
