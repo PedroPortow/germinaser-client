22394e80e036729b82136099d86b986b48f75d36
porto <pedrolportow@gmail.com>
2024-12-03 22:14:47 -0300

    feat: intialized admin/users page

---
+++ src/app/(root)/admin/users/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { BookingsList } from "@/components";
+import { Plus } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { useRouter } from "next/navigation";
+
+export default function Page() {
+  const router = useRouter()
+
+  return (
+    <div className="w-full max-w-[800px]">
+      <Card className=" w-full py-4 px-5 flex flex-col ">
+      <div className="flex flex-col md:flex-row items-start md:items-end justify-between">
+        <div className="flex flex-col">
+          <h1 className="font-semibold text-lg">Reservas</h1>
+          <p className="text-muted-foreground text-sm">Aqui você pode visualizar todas suas reservas</p>
+        </div>
+        <Button 
+          className="text-sm mt-4 md:mt w-full md:w-auto flex items-center" 
+          onClick={() => router.push('/bookings/new')}
+        >
+          <Plus />
+          Nova Reserva
+        </Button>
+      </div>
+      <div className="mt-4">
+        <BookingsList />
+      </div>
+    </Card>
+    </div>
+  );
+}
