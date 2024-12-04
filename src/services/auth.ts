6367c6d5f1f0d5e6ccf4fc5fec837115e7521c5b
porto <pedrolportow@gmail.com>
2024-12-03 21:47:18 -0300

    fix: removed unnecessary axios gambiarra

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,15 +1,5 @@
+import api from './api'
 import { LoginPayload, LoginResponse } from '@/types'
-import axios from 'axios'
-
-// TODO: Fix this bullshit 2
-const api = axios.create({
-  // baseURL: 'http://localhost:3000/',
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-  timeout: 10000,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-})
 
 export const apiPostLogin = ({ email, password }: LoginPayload): Promise<LoginResponse> => (
   api.post('/login', { user: { email, password }})
