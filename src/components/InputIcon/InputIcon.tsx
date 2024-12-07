70323bda96610f82d7686c04ea20e91188883863
porto <pedrolportow@gmail.com>
2024-12-06 23:36:33 -0300

    feat: created inputIcon component

---
+++ src/components/InputIcon/InputIcon.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import React from "react";
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  icon?: React.ReactNode;
+}
+
+const InputIcon = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, icon, ...props }, ref) => {
+    return (
+      <div
+        className={cn(
+          "flex h-10 items-center rounded-md border border-input bg-white pl-3 text-sm ring-offset-background focus-within:ring-1 focus-within:ring-ring focus-within:ring-offset-2",
+          className,
+        )}
+      >
+        {icon}
+        <input
+          {...props}
+          ref={ref}
+          className="w-full p-2 placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
+        />
+      </div>
+    );
+  },
+);
+
+InputIcon.displayName = "InputIcon";
+
+export default InputIcon;
