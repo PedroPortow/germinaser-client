2ffaa6e25e3a87f3aa3a5d088e2ae086a4471901
porto <pedrolportow@gmail.com>
2024-12-07 17:31:57 -0300

    refac(InputIcon): changed icon alignment

--- src/components/InputIcon/InputIcon.tsx
+++ src/components/InputIcon/InputIcon.tsx
@@ -3,8 +3,7 @@
 import { cn } from "@/lib/utils";
 import React from "react";
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   icon?: React.ReactNode;
 }
 
@@ -13,19 +12,23 @@ const InputIcon = React.forwardRef<HTMLInputElement, InputProps>(
     return (
       <div
         className={cn(
-          "flex h-10 items-center rounded-md border border-input bg-white pl-3 text-sm ring-offset-background focus-within:ring-1 focus-within:ring-ring focus-within:ring-offset-2",
-          className,
+          "flex h-10 items-center rounded-md border border-input bg-white text-sm ring-offset-background focus-within:ring-1 focus-within:ring-ring focus-within:ring-offset-2",
+          className
         )}
       >
-        {icon}
         <input
           {...props}
           ref={ref}
-          className="w-full p-2 placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
+          className="flex-1 px-3 py-2 placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
         />
+        {icon && (
+          <div className="pr-3 flex items-center justify-center">
+            {icon}
+          </div>
+        )}
       </div>
     );
-  },
+  }
 );
 
 InputIcon.displayName = "InputIcon";
